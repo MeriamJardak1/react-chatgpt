0eb296d0cc1826c4e711a92568ade584a4c6849b
Meriam <meriam.jardak1@etu.univ-lorraine.fr>
2023-08-07 12:27:09 +0200

    Version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState,useEffect } from "react"
 
-function App() {
+
+const App =() => {
+  const [value,setValue]= useState(null)
+  const [message,setMessage]=useState(null)
+
+  const getMessages = async () =>{
+    const options ={
+      method : "POST",
+      body : JSON.stringify({
+            message: value
+        }),
+      headers: {
+          "content-Type" : "application/json"
+      }
+    }
+    try {
+      const response = await fetch('http://localhost:8000/completions', options)
+      const data = await response.json()
+      console.log(data)
+      setMessage(data.choices[0].message)
+    } catch (error) {
+      console.error(error)
+      
+    }
+
+  }
+  console.log(value)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <section className="side-bar">
+        <button> + New Chat</button>
+        <ul className="history">
+          <li>
+            BLUGH
+          </li>
+        </ul>
+        <nav>
+          <p>Made by Meriam </p>
+        </nav>
+
+      </section>
+      <section className="main" >
+        <h1>MeriGPT</h1>
+        <ul className="feed">
+        </ul>
+        <div className="bottom-section ">
+          <div className="input-container">
+            <input value={value} onChange={(e) => setValue(e.target.value)}/>
+            <div id="submit" onClick ={getMessages} > ► </div>
+          </div>
+          <p className="info">
+          MeriGPT July 31 Version.Free Research Preview. Our goal is to make AI systems more natural and safe to interact with. Your feedback will help us improve
+          </p>
+        </div>
+      </section>
+      
     </div>
   );
 }
 
-export default App;
+export default App
